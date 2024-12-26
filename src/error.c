cfd89a5bc5bba7dd77a17bb3f5cd9a8b2e1bf978
swenn <neoz.tetrel@gmail.com>
2024-12-26 17:32:12 +0100

    \n error code

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: stetrel <stetrel@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 15:56:37 by stetrel           #+#    #+#             */
-/*   Updated: 2024/12/26 15:52:33 by swenn            ###   ########.fr       */
+/*   Updated: 2024/12/26 17:28:26 by swenn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,22 @@ void	error_message(int error)
 {
 	if (error == TOO_FEW_ARGS)
 	{
-		write(2, "Error: too few args provided!\n", 30);
+		write(2, "\nError: too few args provided!\n", 31);
 		exit(0);
 	}
 	if (error == NO_PATH_FOUND)
 	{
-		write(2, "Error: No path found!\n", 22);
+		write(2, "\nError: No path found!\n", 23);
 		exit(0);
 	}
 	if (error == NO_FILES)
 	{
-		write(2, "Error: File don't exist!\n", 24);
+		write(2, "\nError: File don't exist!\n", 25);
 		exit(0);
 	}
 	if (error == NO_COMMAND_FOUND)
 	{
-		write(2, "Error: Command not found!\n", 26);
+		write(2, "\nError: Command not found!\n", 27);
 		exit(0);
 	}
 }
