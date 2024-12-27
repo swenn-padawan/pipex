59aa4b92e86aa47a8ebdd5e4339fe57062eee321
Swenn Tetrel <stetrel@3F3.42angouleme.fr>
2024-12-27 17:02:25 +0100

    check here doc

--- src/bonus/main_bonus.c
+++ src/bonus/main_bonus.c
@@ -6,23 +6,43 @@
 /*   By: stetrel <stetrel@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 04:42:10 by stetrel           #+#    #+#             */
-/*   Updated: 2024/12/27 10:00:48 by stetrel          ###   ########.fr       */
+/*   Updated: 2024/12/27 17:01:54 by stetrel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
 #include <pipex_bonus.h>
+#include <string.h>
+
+int	check_here_doc(char **argv)
+{
+	if (strcmp(argv[1], "here_doc"))
+		return (1);
+	return (0);
+}
+
+char	**check_args(int argc, char **argv, char **envp)
+{
+	if (argc < 5 && check_here_doc(argv))
+		error_message(TOO_FEW_ARGS);
+	if (argc < 7 && !check_here_doc(argv))
+		error_message(TOO_FEW_ARGS);
+	while (*envp && ft_strncmp(*envp, "PATH=", 5))
+		envp++;
+	if (!envp)
+		error_message(NO_PATH_FOUND);
+	return (envp);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
 	t_list	*lst;
 	t_pipex	pipex;
 
-	(void)envp;
+	envp = check_args(argc, argv, envp);
 	lst = ft_list_init(argc, argv);
 	pipex.infile = open(lst->content, O_RDONLY);
-	printf("%s\n", (char *)lst->content);
 	if (pipex.infile == -1)
 		error_message(NO_FILES);
 	pipex.outfile = get_outfile(lst);
+	//while de pipex en parcourant la liste chainees
 }
