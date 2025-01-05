2d2653555c0843b37d31fd0d7bc225c77863e026
Jeremy lorette <jeremy.lorette@outlook.com>
2025-01-05 14:26:26 +0100

    init here_doc

---
+++ src/bonus/here_doc/here_doc.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   here_doc.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/05 13:45:35 by jlorette          #+#    #+#             */
+/*   Updated: 2025/01/05 14:06:06 by jlorette         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <pipex_bonus.h>
+
+void	here_doc(t_env env, int argc, char	*limiter)
+{
+	// read de 0 avec GNL
+	// while jusqua la line comporte le limiter
+	  // parse la commande
+	    // si elle existe exec
+}
--- src/bonus/main_bonus.c
+++ src/bonus/main_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stetrel <stetrel@42angouleme.fr>           +#+  +:+       +#+        */
+/*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 04:42:10 by stetrel           #+#    #+#             */
-/*   Updated: 2025/01/04 12:47:32 by swenn            ###   ########.fr       */
+/*   Updated: 2025/01/05 13:49:53 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	**check_args(int argc, char **argv, char **envp)
 {
 	if (argc < 5 && check_here_doc(argv))
 		error_message(TOO_FEW_ARGS);
-	if (argc < 7 && !check_here_doc(argv))
+	if (argc < 6 && !check_here_doc(argv))
 		error_message(TOO_FEW_ARGS);
 	while (*envp && ft_strncmp(*envp, "PATH=", 5))
 		envp++;
@@ -36,9 +36,14 @@ char	**check_args(int argc, char **argv, char **envp)
 int	main(int argc, char **argv, char **envp)
 {
 	t_env	env;
+	int		flag;
 
 	env.envp = check_args(argc, argv, envp);
 	env.argv = argv;
-	execute_pipeline(env, argc);
+	flag = check_here_doc(argv);
+	if (!flag)
+		here_doc(env, argc);
+	else
+		execute_pipeline(env, argc);
 	return (0);
 }
